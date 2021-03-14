53c0dd11be08a14125a80864c38896b6dea9af4e
Alexis Carrère-Moreno <alcamoru@gmail.com>
2021-03-14 10:11:41 +0100

    Resolving stage 2

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,1 +1,6 @@
-print("Meet a conicoin!")
+n_conicoins = int(input())
+
+print(f"I have {n_conicoins} conicoins.", f"{n_conicoins} "
+                                          f"conicoins cost "
+                                          f"{n_conicoins * 100} dollars.", sep="\n")
+print("I am rich! Yippee!")
