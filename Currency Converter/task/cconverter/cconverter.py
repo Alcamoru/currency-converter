b73e34348b6fdb3ddcf8cdc2f20c83e023b121f3
Alexis Carrère-Moreno <alcamoru@gmail.com>
2021-03-24 17:28:26 +0100

    Resolving stage 4

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,6 +1,8 @@
-n_conicoins = int(input())
+n_conicoins = float(input())
 
-print(f"I have {n_conicoins} conicoins.", f"{n_conicoins} "
-                                          f"conicoins cost "
-                                          f"{n_conicoins * 100} dollars.", sep="\n")
-print("I am rich! Yippee!")
+print(f"I will get {n_conicoins * 2.98} RUB from the sale of {n_conicoins} conicoins.")
+print(f"I will get {n_conicoins * 0.82} ARS from the sale of {n_conicoins} conicoins.")
+print(f"I will get {n_conicoins * 0.17} HNL from the sale of {n_conicoins} conicoins.")
+print(f"I will get {n_conicoins * 1.9622} AUD from the sale of {n_conicoins} conicoins.")
+print(f"I will get {n_conicoins * 0.208} MAD from the sale of {n_conicoins} conicoins.")
+
--- Currency Converter/task/tests.py
+++ Currency Converter/task/tests.py
@@ -5,47 +5,55 @@ from hstest.stage_test import StageTest
 from hstest.test_case import TestCase
 
 
-class TestStage2(StageTest):
+class TestStage4(StageTest):
 
     def generate(self) -> List[TestCase]:
         list_tests = [
-            TestCase(stdin='42', attach=['42', '4200']),
-            TestCase(stdin='75', attach=['75', '7500'])
+            TestCase(stdin='17', attach=[17]),
+            TestCase(stdin='3.5', attach=[3.5]),
+            TestCase(stdin='21', attach=[21]),
+            TestCase(stdin='4.5', attach=[4.5])
 
         ]
+
+        currs = {'rub': 2.98, 'ars': 0.82, 'hnl': 0.17, 'aud': 1.9622, 'mad': 0.208}
+        for test in list_tests:
+            attach_dict = {}
+            for k, v in currs.items():
+                attach_dict[k] = round(v * float(test.input), 2)
+            test.attach.append(attach_dict)
 
         return list_tests
 
     def check(self, reply: str, attach) -> CheckResult:
+        ccoins_att, currs_att = attach
         reply_parsed = reply.strip().split('\n')
-        if len(reply_parsed) != 3:
-            return CheckResult.wrong("Check your output")
-        first_parsed = reply_parsed[0].split()
-        if len(first_parsed) != 4:
-            return CheckResult.wrong("Check your first line")
-        try:
-            amount = int(first_parsed[2])
-        except ValueError:
-            return CheckResult.wrong("Format your output"
-                                     "according to the example")
-        if amount != int(attach[0]):
-            return CheckResult.wrong("The amount of money is wrong")
-        second_parsed = reply_parsed[1].split()
-        if len(second_parsed) != 5:
-            return CheckResult.wrong("Check your second line")
-        try:
-            amount = int(second_parsed[0])
-            dollars = int(second_parsed[3])
-        except ValueError:
-            return CheckResult.wrong("Format your output"
-                                     "according to the example")
-        if amount != int(attach[0]) or dollars != int(attach[1]):
-            return CheckResult.wrong("The amount of monet is wrong")
-        if "i am rich" not in reply_parsed[2].lower():
+        if len(reply_parsed) != 5:
             return CheckResult.wrong("Your output differs from the example")
-
+        for repl in reply_parsed:
+            repl = repl.lower()
+            repl_parsed = repl.strip().split()
+            if len(repl_parsed) != 11:
+                return CheckResult.wrong("Your output differs from the example")
+            try:
+                cur = float(repl_parsed[3])
+                ccoins = float(repl_parsed[-2])
+                key = repl_parsed[4]
+            except (ValueError, KeyError):
+                return CheckResult.wrong("Format your output according to the example")
+            if ccoins != ccoins_att:
+                return CheckResult.wrong("The amount of conicoins is wrong")
+            try:
+                amount_curr = currs_att[key]
+            except KeyError:
+                return CheckResult.wrong("The currency name in the output of your program seems to be wrong:\n"
+                                         "\"{0}\"".format(key.upper()))
+            if abs(amount_curr - cur) > 0.2:
+                return CheckResult.wrong(f"The amount of {key.upper()} is wrong")
+            if not ('i will get' in repl and 'from the sale of' in repl and 'conicoins' in repl):
+                return CheckResult.wrong("Format your output according to the example")
         return CheckResult.correct()
 
 
 if __name__ == '__main__':
-    TestStage2("cconverter.cconverter").run_tests()
+    TestStage4("cconverter.cconverter").run_tests()
